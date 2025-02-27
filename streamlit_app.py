888d1c9bcd76be8095cb34350bdd0ac3da01d374
dharanivasan <dharanivasan2005@gmail.com>
2025-02-27 10:05:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
-st.dataframe(data=moothiefroot_response.json(), use_container_width=True)
+#st.dataframe(data=moothiefroot_response.json(), use_container_width=True)
 
 
 import streamlit as st
